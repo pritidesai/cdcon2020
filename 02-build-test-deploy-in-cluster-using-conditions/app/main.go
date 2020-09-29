35f0d19c2a32a278d940869620716ea0abc472c3
Priti Desai <pdesai@us.ibm.com>
2020-09-29 11:41:00 -0700

    adding conditions use case

---
+++ 02-build-test-deploy-in-cluster-using-conditions/app/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "time"
+)
+
+func main() {
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    t := time.Now()
+    fmt.Fprintf(w, "\n\nHello, %s!", r.URL.Path[1:])
+    fmt.Fprintln(w,"\n\n\nLocal time: ", t.Format(time.UnixDate))
+    fmt.Fprintln(w, "\n\nThis is an app running as part of Tekton Demo for 02-build-test-deploy-in-region-using-conditions :-)")
+}
+
