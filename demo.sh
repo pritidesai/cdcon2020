cb83db1d01a829b2f887747ede38a485e7598dc0
Priti Desai <pdesai@us.ibm.com>
2020-09-29 09:47:08 -0700

    updating demo commands

--- demo.sh
+++ demo.sh
@@ -1,13 +1,15 @@
 open http://127.0.0.1:32426
 
-
+tkn pr list
 tkn pr describe 02-build-test-deploy-in-cluster
 kubectl get pr 02-build-test-deploy-in-cluster -o json | jq .status.skippedTasks
 open http://127.0.0.1:32428
 
+tkn pr list
 tkn pr describe 03-guarded-build-test-deploy
 kubectl get pr 03-guarded-build-test-deploy -o json | jq .status.skippedTasks
 
+tkn pr list
 tkn pr describe 04-build-test-deploy-cleanup
 kubectl get pr 04-build-test-deploy-cleanup -o json | jq .status.skippedTasks
 
